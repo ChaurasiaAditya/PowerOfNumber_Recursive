e3681bcfce037c7c53cb986b81825899cdf658d0
Aditya Chaurasia <chaurasiaaditya755@gmail.com>
2022-09-10 20:53:46 +0530

    created calculatePowerRecursively method in PowerCalculator class

--- src/main/java/com/aditya/PowerCalculator.java
+++ src/main/java/com/aditya/PowerCalculator.java
@@ -6,4 +6,21 @@
 package com.aditya;
 
 public class PowerCalculator {
+
+    /**
+     * This method calculates the power of a base number to an exponent.
+     *
+     * @param base     the base of the power.
+     * @param exponent the exponent of the power.
+     * @return the result of the power or Double.MIN_VALUE if the exponent is negative.
+     */
+    public double calculatePowerRecursively(double base, double exponent) {
+        if (exponent < 0) {
+            return Double.MIN_VALUE;
+        }
+        if (exponent == 0) {
+            return 1;
+        }
+        return base * calculatePowerRecursively(base, exponent - 1);
+    }
 }
